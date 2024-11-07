691683b60fecb162671025783ebd2d24ca03f54d
Rosiele Sousa <rosielesousa49@gmail.com>
2024-11-07 16:55:03 -0300

    eu tentei...

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-let result = ''
+function triangleArea(triangleBase, triangleHeight){
+    return triangleBase * triangleHeight / 2
+}
+
+function rectangleArea(rectangleBase, rectangleBase){
+    return rectangleBase * rectangleBase
+}
+
+function squareArea(sideSquare){
+    return sideSquare * sideSquare
+}
+
+    function areaTrapeze(baseLargest, baseSmallest, trapezoidHeight){
+        return (baseLargest + baseSmallest) * trapezoidHeight / 2
+    }
+    
+    function circleArea(circleRadius){
+        return (circleRadius * circleRadius ) * 3,14  
+    }
 
 do {
     option = prompt(`
-       areas geometricas calculadas ${result.length}
+        calculo de formas geometricas
        voce deseja calcular:
        1 - area do triangulo;
        2 - area do retangulo;
@@ -12,39 +30,40 @@ do {
        6 -  sair;`)
        
         switch(option){
-            case '1':
-            function triangleArea(base, altura){
-                return base * altura / 2
-            }
-            console.log(triangleArea(5, 2))
-            break
-            case '2' :
-                 
-            function rectangleArea(base, altura){
-                return base * altura
-            }
-            console.log(rectangleArea(5, 3))
-            break
-            case '3' : 
-            function squareArea(base){
-                return base * base
-            }
-            console.log(squareArea(5, 5))
-            break
-            case '4' :
-                function areaTrapeze(baseMaior, baseMenor, altura){
-                    return (baseMaior + baseMenor) * altura / 2
-                }
-                console.log(areaTrapeze(10, 10, 2))
-                break
-                case '5' : 
-                function circleArea(raio){
-                    return (raio * raio ) * 3,14  
-                }
-                console.log(circleArea(3, 3))
-                break
-                default :
-                    console.log('saindo do programa...')
+           case '1' :
+            let triangleBase = prompt("Digite a base do triangulo:");
+            let triangleHeight = prompt("Digite a altura do triangulo:");
+            console.log(`A area do triangulo e: ${triangleArea}`)
+            break;
+
+           case '2' :
+            let rectangleBase = prompt("Digite a base do retangulo:");
+            let heightRectangle = prompt("Digite a altura do retangulo:");
+            console.log(`A area do retangulo e: ${rectangleArea}`)
+            break;
+
+           case '3' : 
+           let sideSquare = prompt("Digite o lado do quadrado:");
+           console.log(`A area do circulo e: ${squareArea}`);
+           break;
+
+           case '4' :
+            let baseLargest = prompt("Digite a base maior do trapezio:");
+            let baseSmallest = prompt("Digite a base menor do trapezio:");
+            let trapezoidHeight = prompt("Digite a altura do trapezio:");
+            console.log(`A area do trapezio e: ${areaTrapeze}`)
+            break;
+
+           case '5' :
+            let circleRadius = lerEntrada("Digite o raio do circulo:");
+            console.log(`A area do circulo e: ${circleArea}`)
+            break;
+
+            case '6':
+             console.log('saindo do programa...')
+
+                    default : 
+                    console.log('opcao invalida. tente novamente')
                 
         }
     } while (option !== '6')
