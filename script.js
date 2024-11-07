aa706c307bafd901f8bab2c4bbfa69c2eb904343
Rosiele Sousa <rosielesousa49@gmail.com>
2024-11-07 09:37:41 -0300

    códigos 2.0

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let result = []
+let base = ''
+let altura = ''
+baseMaior = ''
+let baseMenor = ''
+let raio = ''
 let  option = ''
 
 do {
@@ -11,34 +15,44 @@ do {
        4 - area do trapezio;
        5 - area do circulo;
        6 -  sair;`)
+       
 
         switch(option){
-            case '1': 
+            case '1':
+                base = prompt('digite o valor da base:')
+                altura = prompt('digite o valor da altura:')
             function triangleArea(base, altura){
                 return base * altura / 2
             }
             console.log(triangleArea())
             break
-            case '2' : 
+            case '2' :
+                base = prompt('digite o valor da base:')
+                altura = prompt('digite o valor da altura:') 
             function rectangleArea(base, altura){
                 return base * altura
             }
             console.log(rectangleArea)
             break
-            case '3' : 
+            case '3' :
+                base = prompt('digite o valor da base:') 
             function squareArea(base){
                 return base * base
             }
             console.log(squareArea)
             break
             case '4' :
+                baseMaior = prompt('digitye o valor da base maior:')
+                baseMenor = prompt('digite o valor da base menor:')
+                altura = prompt('digite o valor da altura:')
                 function areaTrapeze(baseMaior, baseMenor, altura){
                     return (baseMaior + baseMenor) * altura / 2
                 }
                 console.log(areaTrapeze)
                 break
                 case '5' : 
-                function circleArea(raio, pi){
+                raio = prompt('digite o valor do raio:')
+                function circleArea(raio){
                     return (raio * raio) * 3,14  
                 }
                 console.log(circleArea)
